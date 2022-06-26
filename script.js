815a0db8e95d614944185859252d4f1d2e131842
brandontaylor156 <brandon.david4@outlook.com>
2022-06-26 01:50:25 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+function loadReport(element){
+    alert("Loading weather report for " + element.innerText);
+}
+
+function changeCursor(element){
+    element.style.cursor = "pointer";
+}
+
+function deleteMessage(){ 
+    document.querySelector(".cookie").remove();
+}
+
+function c2f(temp) {
+    return Math.round(9 / 5 * temp + 32);
+}
+
+function f2c(temp) {
+    return Math.round(5 / 9 * (temp - 32));
+}
+
+function changeMetric(element){
+    for (let i = 1; i < 9; i++) {
+        var tempSpan = document.querySelector("#temp" + i);
+        var tempVal = parseInt(tempSpan.innerText);
+        if(element.value == "celsius") {
+            tempSpan.innerText = f2c(tempVal) + "°";
+        }
+        else {
+            tempSpan.innerText = c2f(tempVal) + "°";
+        }
+    }
+}
