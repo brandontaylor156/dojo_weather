1032d1df2fac1a9588e9a4f247121abfc9e16883
brandontaylor156 <brandon.david4@outlook.com>
2022-06-26 01:53:21 -0700

    comment on css file

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ function f2c(temp) {
     return Math.round(5 / 9 * (temp - 32));
 }
 
+// USED CODING DOJO SOLUTION FOR HELP ON THIS FUNCTION
+
 function changeMetric(element){
     for (let i = 1; i < 9; i++) {
         var tempSpan = document.querySelector("#temp" + i);
